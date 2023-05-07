b2292513c91cad456a23302b3ea76945e50a10e1
unknown <l201310@lhr.nu.edu.pk>
2023-05-07 11:15:17 +0500

    Final commit

--- frontend/src/components/Route/Hero/Hero.jsx
+++ frontend/src/components/Route/Hero/Hero.jsx
@@ -5,7 +5,6 @@ import styles from "../../../styles/styles";
 const Hero = () => {
   return (
     <div
-<<<<<<< HEAD
       className={`relative min-h-[70vh] 800px:min-h-[80vh] hero-img-aaa w-full bg-no-repeat ${styles.noramlFlex}`}
       style={{
         backgroundImage:
@@ -13,21 +12,12 @@ const Hero = () => {
       }}
     >
       <div className={`${styles.section} w-[90%]  mlllb 800px:w-[60%]`}>
-=======
-      className={`relative min-h-[70vh] 800px:min-h-[90vh] w-full bg-no-repeat bs ${styles.noramlFlex}`}
-      style={{
-        backgroundImage:
-         "url(https://thuthuatnhanh.com/wp-content/uploads/2020/09/hinh-nen-game-4k-assasin.jpg)",
-      }}
-    >
-      <div className={`${styles.section} w-[90%] 800px:w-[60%] ml`}>
->>>>>>> a39b01ff4eef541431aee654260e3443fc9825cb
         <h1
-          className={`text-[35px] leading-[1.2] 800px:text-[60px] cw text-[#3d3a3a] font-[600] capitalize`}
+          className={`text-[35px] leading-[1.2] 800px:text-[60px] text-[#3d3a3a] font-[600] capitalize`}
         >
           Best Collection for <br /> home Decoration
         </h1>
-        <p className="pt-5 text-[16px] font-[Poppins] font-[400] cw text-[#000000ba]">
+        <p className="pt-5 text-[16px] font-[Poppins] font-[400] text-[#000000ba]">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae,
           assumenda? Quisquam itaque <br /> exercitationem labore vel, dolore
           quidem asperiores, laudantium temporibus soluta optio consequatur{" "}
